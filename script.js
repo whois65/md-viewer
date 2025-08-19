9c33568005851e4d8e15182203e268623878339c
Raffiqi <raffi652009@gmail.com>
2025-08-19 15:48:00 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+let script = `<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+  <script>document.addEventListener("DOMContentLoaded", ()=>{document.querySelectorAll("pre code").forEach(block=>hljs.highlightElement(block))});</script>`;
