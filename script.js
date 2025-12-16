c16bda68bc0fd7242dc5b99836c65dee36524ddf
Raffiqi <raffi652009@gmail.com>
2025-12-16 14:26:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-let script = `<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
-  <script>document.addEventListener("DOMContentLoaded", ()=>{document.querySelectorAll("pre code").forEach(block=>hljs.highlightElement(block))});</script><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
-  <script>hljs.highlightAll();</script><style>pre{background-color: #0d1117;padding: 14px;border-radius: 12px;overflow-x: auto;box-shadow: 0 4px 12px rgba(0, 0, 0, 0.4);margin-bottom: 16px;}code{font-family: "Fira Code", monospace;font-size: 15px;line-height: 1.5;color: #f8f8f2;}</style>`;
